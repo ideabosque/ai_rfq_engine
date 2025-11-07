dcf137545aeea61df0d0ffa28bfa13d15130bd9d
Idea Bosque <ideabosque@gmail.com>
2025-11-07 10:00:14 -0800

    Fix some bugs related to resolve list.

--- ai_rfq_engine/tests/test_ai_rfq_engine.py
+++ ai_rfq_engine/tests/test_ai_rfq_engine.py
@@ -59,8 +59,9 @@ sys.path.insert(0, os.path.join(base_dir, "silvaengine_utility"))
 sys.path.insert(1, os.path.join(base_dir, "silvaengine_dynamodb_base"))
 sys.path.insert(2, os.path.join(base_dir, "ai_rfq_engine"))
 
-from ai_rfq_engine import AIRFQEngine
 from silvaengine_utility import Utility
+
+from ai_rfq_engine import AIRFQEngine
 
 
 def _call_method(
