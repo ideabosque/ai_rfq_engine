4f8d9ddb2abf159ca667996df303186bd78729e8
Idea Bosque <ideabosque@gmail.com>
2025-12-10 19:36:38 -0800

    refactor: update main.py imports and structure

--- ai_rfq_engine/main.py
+++ ai_rfq_engine/main.py
@@ -8,8 +8,8 @@ import logging
 from typing import Any, Dict, List
 
 from graphene import Schema
-
-from silvaengine_dynamodb_base import SilvaEngineDynamoDBBase
+from silvaengine_dynamodb_base import BaseModel
+from silvaengine_utility import Graphql
 
 from .handlers.config import Config
 from .schema import Mutations, Query, type_class
@@ -233,9 +233,18 @@ def deploy() -> List:
     ]
 
 
-class AIRFQEngine(SilvaEngineDynamoDBBase):
+class AIRFQEngine(Graphql):
     def __init__(self, logger: logging.Logger, **setting: Dict[str, Any]) -> None:
-        SilvaEngineDynamoDBBase.__init__(self, logger, **setting)
+        Graphql.__init__(self, logger, **setting)
+
+        if (
+            setting.get("region_name")
+            and setting.get("aws_access_key_id")
+            and setting.get("aws_secret_access_key")
+        ):
+            BaseModel.Meta.region = setting.get("region_name")
+            BaseModel.Meta.aws_access_key_id = setting.get("aws_access_key_id")
+            BaseModel.Meta.aws_secret_access_key = setting.get("aws_secret_access_key")
 
         # Initialize configuration via the Config class
         Config.initialize(logger, **setting)
@@ -254,4 +263,4 @@ class AIRFQEngine(SilvaEngineDynamoDBBase):
             mutation=Mutations,
             types=type_class(),
         )
-        return self.graphql_execute(schema, **params)
+        return self.execute(schema, **params)
